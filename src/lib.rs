c2cbbc97603c70025d238d003a5df6f7d0ae54ce
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-21 10:05:34 +0100

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+mod provider;
+
+use provider::en_US::*;
+
 #[cfg(test)]
 mod tests {
+    use crate::provider::person;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn name_can_be_generated() {
+        let name = person::Person::name("male");
+        println!("{}", name);
+        let empty_string = "";
+        assert_ne!(name, empty_string);
     }
 }
