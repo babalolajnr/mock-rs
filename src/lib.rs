495ec4bffb469729ae1128b456c40e3322b1df38
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-22 08:45:36 +0100

    remove Person struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 mod provider;
 
-use provider::en_US::*;
-
 #[cfg(test)]
 mod tests {
     use crate::provider::person;
 
     #[test]
     fn name_can_be_generated() {
-        let name = person::Person::name("male");
+        let name = person::name("male");
         println!("{}", name);
         let empty_string = "";
         assert_ne!(name, empty_string);
