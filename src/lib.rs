31fc84aff833563d436292cc5ba2ec4595d08817
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-22 12:14:08 +0100

    change name method to first_name function

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,24 @@ mod tests {
     use crate::provider::*;
 
     #[test]
-    fn name_can_be_generated() {
-        let name = person::name("male");
+    fn first_name_can_be_generated() {
+        let name = person::first_name(Some("male"));
         println!("{}", name);
         let empty_string = "";
         assert_ne!(name, empty_string);
     }
 
     #[test]
-    fn female_name_can_be_generated() {
-        let name = person::name("female");
+    fn female_first_name_can_be_generated() {
+        let name = person::first_name(Some("female"));
         let female_names = en_us::person::Person::new().first_name_female;
         println!("{}", name);
         assert!(female_names.contains(&name));
     }
 
     #[test]
-    fn male_or_female_name_can_be_generated() {
-        let name = person::name("");
+    fn male_or_female_first_name_can_be_generated() {
+        let name = person::first_name(None);
         let mut person = en_us::person::Person::new();
         let mut male_female_first_names = person.first_name_female;
         male_female_first_names.append(&mut person.first_name_male);
