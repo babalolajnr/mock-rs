43fe628bdf15788d0a8d90269ea2f7d03f0390c5
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-22 08:45:59 +0100

    remove Person struct

--- src/provider/person.rs
+++ src/provider/person.rs
@@ -1,20 +1,24 @@
 use super::en_us::person;
-
-pub struct Person {}
+use rand::prelude::*;
 
 const GENDER_MALE: &str = "male";
 const GENDER_FEMALE: &str = "female";
 
-impl Person {
-    pub fn name(gender: &str) -> &str {
-        if gender == GENDER_FEMALE {
-            let person = person::Person::new();
-            person.first_name_male[0]
-        } else if gender == GENDER_MALE {
-            let person = person::Person::new();
-            person.first_name_male[0]
-        }else{
-            panic!("Gender must be male or female")
-        }
+pub fn name(gender: &str) -> &str {
+    if gender == GENDER_FEMALE {
+        let person = person::Person::new();
+        let random_number = generate_random_number(&person.first_name_male);
+        person.first_name_male[random_number]
+    } else if gender == GENDER_MALE {
+        let person = person::Person::new();
+        let random_number = generate_random_number(&person.first_name_male);
+        person.first_name_male[random_number]
+    } else {
+        panic!("Gender must be male or female")
     }
+}
+
+fn generate_random_number(arr: &[&str]) -> usize {
+    let random_number = thread_rng().gen_range(1..arr.len());
+    random_number
 }
