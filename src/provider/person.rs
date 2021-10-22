8efd43dafe07f42c073054e651d398f39b3f247b
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-22 12:28:12 +0100

    change if else syntax to pattern mathcing syntax

--- src/provider/person.rs
+++ src/provider/person.rs
@@ -4,37 +4,26 @@ use rand::prelude::*;
 const GENDER_MALE: &str = "male";
 const GENDER_FEMALE: &str = "female";
 
-// pub fn name(gender: &str) -> &str {
-    // let mut person = person::Person::new();
-
-    // if gender == GENDER_FEMALE {
-    //     let random_number = generate_random_number(&person.first_name_female);
-    //     person.first_name_female[random_number]
-    // } else if gender == GENDER_MALE {
-    //     let random_number = generate_random_number(&person.first_name_male);
-    //     person.first_name_male[random_number]
-    // } else {
-    //     let mut male_female_first_names = person.first_name_male;
-    //     male_female_first_names.append(&mut person.first_name_female);
-    //     let random_number = generate_random_number(&male_female_first_names);
-    //     male_female_first_names[random_number]
-    // }
-// }
+// pub fn name(gender: &str) -> &str {}
 
 pub fn first_name(gender: Option<&str>) -> &str {
     let mut person = person::Person::new();
 
-    if gender == Some(GENDER_FEMALE) {
-        let random_number = generate_random_number(&person.first_name_female);
-        person.first_name_female[random_number]
-    } else if gender == Some(GENDER_MALE) {
-        let random_number = generate_random_number(&person.first_name_male);
-        person.first_name_male[random_number]
-    } else {
-        let mut male_female_first_names = person.first_name_male;
-        male_female_first_names.append(&mut person.first_name_female);
-        let random_number = generate_random_number(&male_female_first_names);
-        male_female_first_names[random_number]
+    match gender {
+        Some(GENDER_FEMALE) => {
+            let random_number = generate_random_number(&person.first_name_female);
+            person.first_name_female[random_number]
+        }
+        Some(GENDER_MALE) => {
+            let random_number = generate_random_number(&person.first_name_male);
+            person.first_name_male[random_number]
+        }
+        _ => {
+            let mut male_female_first_names = person.first_name_male;
+            male_female_first_names.append(&mut person.first_name_female);
+            let random_number = generate_random_number(&male_female_first_names);
+            male_female_first_names[random_number]
+        }
     }
 }
 
