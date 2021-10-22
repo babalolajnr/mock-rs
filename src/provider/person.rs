cee986dc4b2b811e84d386c94fe6b6337e74e39c
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-22 12:32:42 +0100

    add wildcard match

--- src/provider/person.rs
+++ src/provider/person.rs
@@ -4,7 +4,9 @@ use rand::prelude::*;
 const GENDER_MALE: &str = "male";
 const GENDER_FEMALE: &str = "female";
 
-// pub fn name(gender: &str) -> &str {}
+// pub fn name(gender: &str) -> &str {
+
+// }
 
 pub fn first_name(gender: Option<&str>) -> &str {
     let mut person = person::Person::new();
@@ -18,11 +20,14 @@ pub fn first_name(gender: Option<&str>) -> &str {
             let random_number = generate_random_number(&person.first_name_male);
             person.first_name_male[random_number]
         }
-        _ => {
+        None => {
             let mut male_female_first_names = person.first_name_male;
             male_female_first_names.append(&mut person.first_name_female);
             let random_number = generate_random_number(&male_female_first_names);
             male_female_first_names[random_number]
+        }
+        _ => {
+            panic!("Unknown gender")
         }
     }
 }
