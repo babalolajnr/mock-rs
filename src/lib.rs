0e801130fa33f0c9841d8efd12283d2c8607b0dc
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-22 09:03:52 +0100

    add test: female_name_can_be_generated

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod provider;
 
 #[cfg(test)]
 mod tests {
-    use crate::provider::person;
+    use crate::provider::*;
 
     #[test]
     fn name_can_be_generated() {
@@ -11,4 +11,14 @@ mod tests {
         let empty_string = "";
         assert_ne!(name, empty_string);
     }
+
+    #[test]
+    fn female_name_can_be_generated() {
+        let name = person::name("female");
+        let female_names = en_us::person::Person::new().first_name_female;
+        println!("{}", name);
+        assert!(female_names.contains(&name));
+    }
+
+
 }
