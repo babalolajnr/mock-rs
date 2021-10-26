39ca0a26c22deb3db2e751d58f7fbe16484b186b
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-26 16:27:41 +0100

    add test: female_name_can_be_generated

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod tests {
         let name = person::first_name(Some("male"));
         let male_names = en_us::person::Person::new().first_name_male;
         println!("{}", name);
-        assert!(male_names.contains(&name));
+        assert!(male_names.contains(&name.as_str()));
     }
 
     #[test]
@@ -17,7 +17,7 @@ mod tests {
         let name = person::first_name(Some("female"));
         let female_names = en_us::person::Person::new().first_name_female;
         println!("{}", name);
-        assert!(female_names.contains(&name));
+        assert!(female_names.contains(&name.as_str()));
     }
 
     #[test]
@@ -27,12 +27,21 @@ mod tests {
         let mut male_female_first_names = person.first_name_female;
         male_female_first_names.append(&mut person.first_name_male);
         println!("{}", name);
-        assert!(male_female_first_names.contains(&name));
+        assert!(male_female_first_names.contains(&name.as_str()));
     }
 
     #[test]
     #[should_panic(expected = "Unknown gender")]
     fn unknown_gender_will_panic() {
         person::first_name(Some("lorem"));
+    }
+
+    #[test]
+    fn female_name_can_be_generated() {
+        let name = person::name(Some("female"));
+        let female_first_names = en_us::person::Person::new().first_name_female;
+        println!("{}", name);
+        let name: Vec<&str> = name.rsplit(" ").collect();
+        assert!(female_first_names.contains(&name[1]));
     }
 }
