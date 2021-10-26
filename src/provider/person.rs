49dabe08b0fa2fc1c5ae00dc4bb35b9fcdc331a9
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-26 16:46:34 +0100

    refactor tests

--- src/provider/person.rs
+++ src/provider/person.rs
@@ -74,21 +74,21 @@ mod tests {
 
     #[test]
     fn male_first_name_can_be_generated() {
-        let name = person::first_name(Some("male"));
+        let name = super::first_name(Some("male"));
         let male_names = en_us::person::Person::new().first_name_male;
         assert!(male_names.contains(&name.as_str()));
     }
 
     #[test]
     fn female_first_name_can_be_generated() {
-        let name = person::first_name(Some("female"));
+        let name = super::first_name(Some("female"));
         let female_names = en_us::person::Person::new().first_name_female;
         assert!(female_names.contains(&name.as_str()));
     }
 
     #[test]
     fn male_or_female_first_name_can_be_generated() {
-        let name = person::first_name(None);
+        let name = super::first_name(None);
         let mut person = en_us::person::Person::new();
         let mut male_female_first_names = person.first_name_female;
         male_female_first_names.append(&mut person.first_name_male);
@@ -103,7 +103,7 @@ mod tests {
 
     #[test]
     fn female_name_can_be_generated() {
-        let name = person::name(Some("female"));
+        let name = super::name(Some("female"));
         let female_first_names = en_us::person::Person::new().first_name_female;
         let name: Vec<&str> = name.rsplit(" ").collect();
         assert!(female_first_names.contains(&name[1]));
@@ -111,7 +111,7 @@ mod tests {
 
     #[test]
     fn male_name_can_be_generated() {
-        let name = person::name(Some("male"));
+        let name = super::name(Some("male"));
         let male_first_names = en_us::person::Person::new().first_name_male;
         let name: Vec<&str> = name.rsplit(" ").collect();
         assert!(male_first_names.contains(&name[1]));
