6af0b1721c8b78822c447bd1d23aff71241bf355
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-26 16:31:59 +0100

    remove println! statements

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ mod tests {
     fn male_first_name_can_be_generated() {
         let name = person::first_name(Some("male"));
         let male_names = en_us::person::Person::new().first_name_male;
-        println!("{}", name);
         assert!(male_names.contains(&name.as_str()));
     }
 
@@ -16,7 +15,6 @@ mod tests {
     fn female_first_name_can_be_generated() {
         let name = person::first_name(Some("female"));
         let female_names = en_us::person::Person::new().first_name_female;
-        println!("{}", name);
         assert!(female_names.contains(&name.as_str()));
     }
 
@@ -26,7 +24,6 @@ mod tests {
         let mut person = en_us::person::Person::new();
         let mut male_female_first_names = person.first_name_female;
         male_female_first_names.append(&mut person.first_name_male);
-        println!("{}", name);
         assert!(male_female_first_names.contains(&name.as_str()));
     }
 
@@ -40,7 +37,6 @@ mod tests {
     fn female_name_can_be_generated() {
         let name = person::name(Some("female"));
         let female_first_names = en_us::person::Person::new().first_name_female;
-        println!("{}", name);
         let name: Vec<&str> = name.rsplit(" ").collect();
         assert!(female_first_names.contains(&name[1]));
     }
@@ -49,7 +45,6 @@ mod tests {
     fn male_name_can_be_generated() {
         let name = person::name(Some("male"));
         let male_first_names = en_us::person::Person::new().first_name_male;
-        println!("{}", name);
         let name: Vec<&str> = name.rsplit(" ").collect();
         assert!(male_first_names.contains(&name[1]));
     }
