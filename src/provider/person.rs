124f241dc6513c51c1021d568a496cf262ef7d62
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-26 16:43:06 +0100

    move tests here

--- src/provider/person.rs
+++ src/provider/person.rs
@@ -58,7 +58,62 @@ pub fn first_name(gender: Option<&str>) -> String {
     }
 }
 
+// fn first_name_male() -> String {
+//     let random_index = generate_random_index(&person.first_name_female);
+//     person.first_name_female[random_index].to_string()
+// }
+
 fn generate_random_index(arr: &[&str]) -> usize {
     let random_index = thread_rng().gen_range(1..arr.len());
     random_index
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::provider::*;
+
+    #[test]
+    fn male_first_name_can_be_generated() {
+        let name = person::first_name(Some("male"));
+        let male_names = en_us::person::Person::new().first_name_male;
+        assert!(male_names.contains(&name.as_str()));
+    }
+
+    #[test]
+    fn female_first_name_can_be_generated() {
+        let name = person::first_name(Some("female"));
+        let female_names = en_us::person::Person::new().first_name_female;
+        assert!(female_names.contains(&name.as_str()));
+    }
+
+    #[test]
+    fn male_or_female_first_name_can_be_generated() {
+        let name = person::first_name(None);
+        let mut person = en_us::person::Person::new();
+        let mut male_female_first_names = person.first_name_female;
+        male_female_first_names.append(&mut person.first_name_male);
+        assert!(male_female_first_names.contains(&name.as_str()));
+    }
+
+    #[test]
+    #[should_panic(expected = "Unknown gender")]
+    fn unknown_gender_will_panic() {
+        person::first_name(Some("lorem"));
+    }
+
+    #[test]
+    fn female_name_can_be_generated() {
+        let name = person::name(Some("female"));
+        let female_first_names = en_us::person::Person::new().first_name_female;
+        let name: Vec<&str> = name.rsplit(" ").collect();
+        assert!(female_first_names.contains(&name[1]));
+    }
+
+    #[test]
+    fn male_name_can_be_generated() {
+        let name = person::name(Some("male"));
+        let male_first_names = en_us::person::Person::new().first_name_male;
+        let name: Vec<&str> = name.rsplit(" ").collect();
+        assert!(male_first_names.contains(&name[1]));
+    }
 }
