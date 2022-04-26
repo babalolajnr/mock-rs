65dbdf956f2bbdbb8ba35b6865099035cdd9a2a1
Abdulqudduus <babalolajnr@gmail.com>
2022-04-26 14:38:04 +0100

    add base address provider

---
+++ src/provider/address.rs
@@ -0,0 +1,15 @@
+struct Address{
+    city_suffix: Vec<&'a str>,
+    street_suffix: Vec<&'a str>,
+    city_formats: Vec<&'a str>,
+    street_name_formats: Vec<&'a str>,
+    street_address_formats: Vec<&'a str>,
+    address_formats: Vec<&'a str>,
+    building_number: Vec<&'a str>,
+    post_code: Vec<&'a str>,
+    country: Vec<&'a str>
+}
+
+impl Address{
+    
+}
