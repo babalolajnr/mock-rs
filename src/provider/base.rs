1fe3a36cc02a4a37992a594b50bb5099bf559678
Abdulqudduus <babalolajnr@gmail.com>
2022-04-27 15:12:13 +0100

    refactory numerify to work with strings that have mixed characters

--- src/provider/base.rs
+++ src/provider/base.rs
@@ -12,18 +12,22 @@ pub trait Base {
         &arr[random_index]
     }
 
-    /// Generates a random string that matches the
-    /// length of the given string containing "#"s
+    /// Replaces all occurences of '#'s with a random number
     fn numerify(string: Option<&str>) -> String {
-        let string = string.unwrap_or("###");
+        let string = string.unwrap_or("####");
 
         let charset: &[u8] = b"0123456789";
         let mut rng = rand::thread_rng();
 
-        let random_string: String = (0..string.len())
-            .map(|_| {
-                let idx = rng.gen_range(0..charset.len());
-                charset[idx] as char
+        let random_string = string
+            .chars()
+            .map(|i| {
+                if i == '#' {
+                    let idx = rng.gen_range(0..charset.len());
+                    charset[idx] as char
+                } else {
+                    i
+                }
             })
             .collect();
 
@@ -34,10 +38,9 @@ pub trait Base {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn numerify_works() {
-
         let string = "####";
         struct Test;
         impl Base for Test {}
@@ -48,13 +51,22 @@ mod tests {
 
     #[test]
     fn numerify_works_with_no_string() {
-
         let string = None;
         struct Test;
         impl Base for Test {}
 
         let result = Test::numerify(string);
-        assert_eq!(result.len(), 3);
+        assert_eq!(result.len(), 4);
+    }
+
+    #[test]
+    fn numerify_works_with_string_of_hashes_mixed_with_other_chars() {
+        let string = "Hello #####";
+        struct Test;
+        impl Base for Test {}
+
+        let result = Test::numerify(Some(string));
+        assert_eq!(result.len(), string.len());
     }
 
     #[test]
