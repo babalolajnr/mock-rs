34fe293c39b5e03c0aad54ffdbe4001180465f81
Abdulqudduus <babalolajnr@gmail.com>
2022-04-27 14:51:28 +0100

    add tests

--- src/provider/base.rs
+++ src/provider/base.rs
@@ -1,10 +1,12 @@
 use rand::{thread_rng, Rng};
 pub trait Base {
+    /// Returns a index from array/vector
     fn generate_random_index<T>(arr: &[T]) -> usize {
         let random_index = thread_rng().gen_range(1..arr.len());
         random_index
     }
 
+    /// Returns a random element an array/vector
     fn random_element<'a, T: ?Sized>(arr: &'a Vec<&'a T>) -> &'a T {
         let random_index = Self::generate_random_index(&arr);
         &arr[random_index]
@@ -31,9 +33,10 @@ pub trait Base {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    
     #[test]
     fn numerify_works() {
-        use super::*;
 
         let string = "####";
         struct Test;
@@ -41,5 +44,36 @@ mod tests {
 
         let result = Test::numerify(Some(string));
         assert_eq!(result.len(), string.len());
+    }
+
+    #[test]
+    fn numerify_works_with_no_string() {
+
+        let string = None;
+        struct Test;
+        impl Base for Test {}
+
+        let result = Test::numerify(string);
+        assert_eq!(result.len(), 3);
+    }
+
+    #[test]
+    fn generate_random_index_works() {
+        let arr = vec![1, 2, 3, 4, 5];
+        struct Test;
+        impl Base for Test {}
+
+        let result = Test::generate_random_index(&arr);
+        assert!(result < arr.len());
+    }
+
+    #[test]
+    fn random_element_works() {
+        let arr = vec!["a", "b", "c", "d", "e"];
+        struct Test;
+        impl Base for Test {}
+
+        let result = Test::random_element(&arr);
+        assert!(arr.contains(&result));
     }
 }
