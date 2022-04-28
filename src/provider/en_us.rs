c445d88d8a62a4dcfb035bbd879f4436b8c26eaf
Abdulqudduus <babalolajnr@gmail.com>
2022-04-28 15:23:11 +0100

    create EnUS struct

--- src/provider/en_us.rs
+++ src/provider/en_us.rs
@@ -1,2 +1,18 @@
+use super::locale::Locale;
+
 pub mod person;
 pub mod provider;
+
+pub(crate) struct EnUS {}
+
+impl Locale for EnUS {
+    fn name<'a>(&self) -> &'a str {
+        "en_us"
+    }
+}
+
+impl EnUS {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
