4e81216af6331795e9db6b87974e9d771ccd82b7
Abdulqudduus <babalolajnr@gmail.com>
2022-04-28 15:22:54 +0100

    create provider trait

--- src/provider.rs
+++ src/provider.rs
@@ -1,10 +1,10 @@
+mod address;
 pub mod base;
 pub mod en_us;
+pub mod formatter;
+pub mod locale;
 pub mod person;
-mod address;
 
-pub enum Locale {
-    EnUS,
+pub trait Provider {
+    fn name<'a>(&self) -> &'a str;
 }
-
-pub trait Provider {}
