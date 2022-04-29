ac34f7b00120870f6e44ebb3112c2078a1b5d8c6
Abdulqudduus <babalolajnr@gmail.com>
2022-04-29 17:47:02 +0100

    remove format trait implementation

--- src/provider/person.rs
+++ src/provider/person.rs
@@ -1,67 +1,56 @@
-use super::Provider;
-
 enum Gender {
     Male,
     Female,
 }
-pub(crate) struct Person {
+pub struct Person {
     gender: Gender,
 }
 
-trait Format<'a> {
-    fn title_format() -> Vec<&'a str> {
-        vec!["{{title_male}}", "{{title_female}}"]
-    }
-
-    fn first_name_format() -> Vec<&'a str> {
-        vec!["{{first_name_male}}", "{{first_name_female}}"]
-    }
-
-    fn male_name_formats() -> Vec<&'a str> {
-        vec!["{{first_name_male}} {{last_name}}"]
-    }
-
-    fn female_name_formats() -> Vec<&'a str> {
-        vec!["{{first_name_female}} {{last_name}}"]
-    }
-
-    fn first_name_male() -> &'static str {
-        "John"
-    }
-
-    fn first_name_female() -> &'static str {
-        "Jane"
-    }
-
-    fn last_name() -> &'static str {
-        "Doe"
-    }
-
-    fn title_male() -> Vec<&'a str> {
-        vec!["Mr.", "Dr.", "Prof."]
-    }
-
-    fn title_female() -> Vec<&'a str> {
-        vec!["Mrs.", "Ms.", "Miss", "Dr.", "Prof."]
-    }
-}
-
-impl<'a> Format<'a> for Person {}
-
-impl Provider for Person {
-    fn name<'a>(&self) -> &'a str {
-        "person"
-    }
-}
 
 impl<'a> Person {
-    pub fn first_name() -> &'a str {
+    pub fn first_name(&self) -> &'a str {
         "John"
     }
+    
     pub fn new() -> Self {
         Self {
             gender: Gender::Male,
         }
     }
-}
 
+    pub fn title_format(&self) -> Vec<&'a str> {
+        vec!["{{title_male}}", "{{title_female}}"]
+    }
+
+    pub fn first_name_format(&self) -> Vec<&'a str> {
+        vec!["{{first_name_male}}", "{{first_name_female}}"]
+    }
+
+    pub fn male_name_formats(&self) -> Vec<&'a str> {
+        vec!["{{first_name_male}} {{last_name}}"]
+    }
+
+    pub fn female_name_formats(&self) -> Vec<&'a str> {
+        vec!["{{first_name_female}} {{last_name}}"]
+    }
+
+    pub fn first_name_male(&self) -> &'static str {
+        "John"
+    }
+
+    pub fn first_name_female(&self) -> &'static str {
+        "Jane"
+    }
+
+    pub fn last_name(&self) -> &'static str {
+        "Doe"
+    }
+
+    pub fn title_male(&self) -> Vec<&'a str> {
+        vec!["Mr.", "Dr.", "Prof."]
+    }
+
+    pub fn title_female(&self) -> Vec<&'a str> {
+        vec!["Mrs.", "Ms.", "Miss", "Dr.", "Prof."]
+    }
+}
