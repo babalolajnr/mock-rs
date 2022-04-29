418c7c93e961f215348e4a311de1802f150db2a4
Abdulqudduus <babalolajnr@gmail.com>
2022-04-29 17:45:21 +0100

    add tests module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 mod factory;
-mod generator;
 mod provider;
+
+#[cfg(test)]
+mod tests{
+    use crate::factory;
+
+    #[test]
+    fn can_get_fake_name() {
+        let mocker = factory::Factory::new();
+        
+        let name = mocker.person.first_name();
+
+        assert!(name.len() > 0);
+        println!("{}", name);
+    }
+}
