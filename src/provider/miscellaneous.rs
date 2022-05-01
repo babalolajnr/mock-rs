c9977038ba32f14121386fc5898306b8768f38c7
Abdulqudduus <babalolajnr@gmail.com>
2022-05-01 11:50:31 +0100

    implement boolean

--- src/provider/miscellaneous.rs
+++ src/provider/miscellaneous.rs
@@ -5,8 +5,14 @@ use rand::Rng;
 pub struct Miscellaneous{}
 
 impl Miscellaneous{
+    
+    /// Return a boolean, true or false.
+    ///
+    /// chance_of_getting_true between 0 (always get false) and 100 (always get true)
     pub fn boolean(chance_of_getting_true: Option<u8>) -> bool {
-        todo!()
+        let chance_of_getting_true = chance_of_getting_true.unwrap_or(50);
+        
+        Self::number_between(Some(0), Some(100)) <= chance_of_getting_true.into()
     }
 
     /// Returns a random number between int_1 and int_2 (any order)
@@ -26,3 +32,5 @@ impl Miscellaneous{
         rand::thread_rng().gen_range(min..max)
     }
 }
+
+// #[cfg(test)]
