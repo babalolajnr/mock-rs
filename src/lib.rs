429140244a46a2fd3508141e0da173fc39c98f36
Abdulqudduus <babalolajnr@gmail.com>
2022-05-02 14:29:38 +0100

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,54 @@
-mod factory;
+pub mod error;
 mod provider;
 
 #[cfg(test)]
-mod tests{
-    use crate::factory;
+mod tests {
+    use crate::provider::{
+        en_us::person::Person as en_us_person,
+        person::{self, PersonTrait},
+        address,
+        shared_formats::SharedFormats,
+    };
+    
+    #[test]
+    fn can_get_fake_first_name() {
+        let first_name = person::Person::new().first_name(None);
+        assert!(first_name.len() > 0);
+    }
+
+    #[test]
+    fn can_get_fake_last_name() {
+        let last_name = person::Person::new().last_name();
+        assert!(last_name.len() > 0);
+    }
 
     #[test]
     fn can_get_fake_name() {
-        let mocker = factory::Factory::new();
-        
-        let name = mocker.person.first_name();
-
+        let name = person::Person::new().name(None);
         assert!(name.len() > 0);
-        println!("{}", name);
     }
+
+    #[test]
+    fn can_get_fake_en_us_name() {
+        let name = en_us_person::new().name(None);
+        assert!(name.len() > 0);
+    }
+
+    #[test]
+    fn can_get_fake_title() {
+        let title = person::Person::new().title(None);
+        assert!(title.len() > 0);
+    }
+
+    #[test]
+    fn can_get_fake_en_us_title() {
+        let title = en_us_person::new().title(None);
+        assert!(title.len() > 0);
+    }
+
+    // #[test]
+    // fn can_get_fake_address() {
+    //     let address = address::Address::new().;
+    //     assert!(address.len() > 0);
+    // }
 }
