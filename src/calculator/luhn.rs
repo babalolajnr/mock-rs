ffbc8865e46c65e8880c79734ac2a9d807f54d41
Abdulqudduus <babalolajnr@gmail.com>
2022-05-21 21:13:21 +0100

    feat: add compute check digit

--- src/calculator/luhn.rs
+++ src/calculator/luhn.rs
@@ -9,13 +9,29 @@ fn checksum(number: &str) -> u32 {
     sum % 10
 }
 
+fn compute_check_digit(partial_number: &str) -> String {
+    let check_digit = 10 - checksum(format!("{}{}", partial_number, "0").as_str());
+
+    if check_digit == 0 {
+        return 0.to_string();
+    }
+
+    format!("{}", check_digit)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::checksum;
+    use super::{checksum, compute_check_digit};
 
     #[test]
     fn test_checksum() {
-        let checksum = checksum("0787686577456576");
-        println!("{}", checksum)
+        let checksum = checksum("234512");
+        assert_eq!(checksum, 4);
+    }
+
+    #[test]
+    fn test_compute_check_digit() {
+        let check_digit = compute_check_digit("234512");
+        assert_eq!(check_digit, "2");
     }
 }
