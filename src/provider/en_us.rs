29f286668dbff38696c3c50c8fc0665d4a354089
Abdulqudduus <babalolajnr@gmail.com>
2022-06-19 20:07:30 +0100

    feat: re-export structs

--- src/provider/en_us.rs
+++ src/provider/en_us.rs
@@ -3,3 +3,9 @@ pub mod address;
 pub mod company;
 pub mod payment;
 pub mod phone_number;
+
+pub use person::Person;
+pub use address::Address;
+pub use company::Company;
+pub use payment::Payment;
+pub use phone_number::PhoneNumber;
