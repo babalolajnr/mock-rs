01ceb92ab89790f30a17674610e63d018a1ad91e
Abdulqudduus <babalolajnr@gmail.com>
2022-06-19 20:07:09 +0100

    refactor: change en_us module export

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 mod calculator;
 
 pub mod provider;
-pub use provider::en_us::{
-    address::Address, company::Company, payment::Payment, person::Person, phone_number::PhoneNumber,
-};
+
+pub use provider::en_us;
+
 pub use provider::{
     address::AddressTrait,
     barcode::BarcodeTrait,
@@ -13,19 +13,3 @@ pub use provider::{
     person::{Gender, PersonTrait},
     phone_number::PhoneNumberTrait,
 };
-
-// mod tests {
-// use crate::provider::{
-//     en_us::person::Person,
-//     person::{Gender, PersonTrait},
-// };
-
-// use super::*;
-
-// #[test]
-// fn test_can_get_random_name() {
-//     let en_us_person = Person::new();
-//     let name = en_us_person.name(Some(Gender::Male));
-//     assert!(name.len() > 0);
-// }
-// }
