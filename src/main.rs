9ac3df644b1b2d5e9878b46e36e5cefb4b56b3d6
Abdulqudduus <babalolajnr@gmail.com>
2022-07-05 20:31:53 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,5 @@
 // use mock_rs::{Barcode, BarcodeTrait};
 
 fn main() {
-    // let first_name = Person::new().first_name(Some(Gender::Male));
 
-    // println!("{}", first_name);
-    // let ean = Barcode::ean(20);
-    // println!("{}", ean);
 }
