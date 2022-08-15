45eb01d089afd238e8e4fdaacc82ee475d8aa6be
Abdulqudduus <babalolajnr@gmail.com>
2022-08-15 06:02:31 +0100

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 
 // use mock_rs::{Color, Barcode, BarcodeTrait, ColorTrait};
 
+use mock_rs::{en_us::Person, PersonTrait, Gender};
+
 
 fn main() {
     // let color = Color::hex_color();
     // println!("{}", color);
     // let barcode = Barcode::
+    let en_us_person = Person::new(); 
+    let male_name = en_us_person.name(Some(Gender::Male));
+    println!("{}", male_name);
+
+    
 }
